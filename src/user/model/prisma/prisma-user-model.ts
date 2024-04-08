88d9ec9f3224bf5356f892ebce637b14929cb312
Marcos Fajoli <markosalmeidaa@gmail.com>
2024-04-08 19:34:11 -0300

    feat: add getBy user and formatUser function

--- src/user/model/prisma/prisma-user-model.ts
+++ src/user/model/prisma/prisma-user-model.ts
@@ -9,6 +9,20 @@ import { AlterUserBody, CreateUserBody, FormatedUser } from '../../dto/_index';
 export class PrismaUserModel implements UserModel {
   constructor(private prisma: PrismaService) {}
 
+  private formatUserReturn(users: User[] | User): FormatedUser[] {
+    if (Array.isArray(users)) {
+      const formatedUsers: FormatedUser[] = users.map((user) => {
+        const { password, ...result } = user;
+        return result;
+      });
+
+      return formatedUsers;
+    } else {
+      const { password, ...result } = users as User;
+      return [result];
+    }
+  }
+
   async create(data: CreateUserBody): Promise<FormatedUser> {
     const uuid = randomUUID();
     const user: User = { ...data, id: uuid };
@@ -17,20 +31,21 @@ export class PrismaUserModel implements UserModel {
       data: user,
     });
 
-    const { password, ...result } = created;
-
-    return result;
+    return this.formatUserReturn(created)[0];
   }
 
   async get(): Promise<FormatedUser[]> {
     const users = await this.prisma.user.findMany();
 
-    const FormatedUsers: FormatedUser[] = users.map((user) => {
-      const { password, ...userWithoutPassword } = user;
-      return userWithoutPassword;
+    return this.formatUserReturn(users);
+  }
+
+  async getBy(filter: Partial<User>): Promise<FormatedUser[]> {
+    const users = await this.prisma.user.findMany({
+      where: filter,
     });
 
-    return FormatedUsers;
+    return this.formatUserReturn(users);
   }
 
   async getOne(id: UUID): Promise<FormatedUser | null> {
@@ -52,18 +67,16 @@ export class PrismaUserModel implements UserModel {
       },
     });
 
-    const { password, ...result } = user;
-
-    return result;
+    return this.formatUserReturn(user)[0];
   }
 
-  async delete(id: UUID): Promise<User> {
+  async delete(id: UUID): Promise<FormatedUser> {
     const response = await this.prisma.user.delete({
       where: {
         id: id,
       },
     });
 
-    return response;
+    return this.formatUserReturn(response)[0];
   }
 }
--- src/user/model/user-model.ts
+++ src/user/model/user-model.ts
@@ -1,9 +1,11 @@
 import { UUID } from 'node:crypto';
 import { AlterUserBody, CreateUserBody, FormatedUser } from '../dto/_index';
+import { User } from '@prisma/client';
 
 export abstract class UserModel {
   abstract create(data: CreateUserBody): Promise<FormatedUser>;
   abstract get(): Promise<FormatedUser[]>;
+  abstract getBy(filter: Partial<User>): Promise<FormatedUser[]>;
   abstract getOne(id: UUID): Promise<FormatedUser | null>;
   abstract update(data: AlterUserBody): Promise<FormatedUser>;
   abstract delete(id: UUID): Promise<any>;
