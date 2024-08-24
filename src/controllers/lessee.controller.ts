d9f4bf8f3ff2813a5b416b91d28f13ce9f38e42e
Joaby Oliveira <joabyoliveira2004@gmail.com>
2024-08-24 13:19:32 -0300

    feat: add error handling

--- src/controllers/lessee.controller.ts
+++ src/controllers/lessee.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { LesseeService } from '../services/lessee.service';
 import { CreateLesseeDTO } from 'src/dto/lessee.dto';
 
@@ -8,9 +14,21 @@ export class LesseeController {
 
   @Post()
   async createLessee(@Body() lessee: CreateLesseeDTO) {
-    console.log(lessee);
-    const createdLessee = await this.lesseeService.createLessee(lessee);
+    try {
+      const createdLessee = await this.lesseeService.createLessee(lessee);
 
-    return createdLessee;
+      return {
+        message: 'Conta criada com sucesso',
+        data: createdLessee,
+      };
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new BadRequestException('Endereço e-mail indisponível');
+      } else {
+        throw new InternalServerErrorException(
+          'Algum erro inesperado aconteceu, tente novamente mais tarde',
+        );
+      }
+    }
   }
 }
