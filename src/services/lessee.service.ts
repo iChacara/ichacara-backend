9f19256d7ce117d7dc1e9f524a4eb1387546674e
Joaby Oliveira <joabyoliveira2004@gmail.com>
2024-08-24 13:02:45 -0300

    feat: hash password

--- src/services/lessee.service.ts
+++ src/services/lessee.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class LesseeService {
   constructor(private prisma: PrismaService) {}
 
-  public createLessee(lesseeData: { email: string; password: string }) {
+  public async createLessee(lesseeData: { email: string; password: string }) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(lesseeData.password, saltOrRounds);
+
     return this.prisma.lessee.create({
       data: {
         user: {
           create: {
             email: lesseeData.email,
-            password: lesseeData.password,
+            password: hashedPassword,
             type: 'lessee',
           },
         },
